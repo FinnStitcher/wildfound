aa08318006f743b1eb6d0ced1dc64cd759b71964
Finn Stitcher <foundfootage@protonmail.com>
2022-10-10 18:56:01 -0700

    geo routes

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,11 +1,26 @@
 const router = require('express').Router();
 
+// realms
 router.get('/realms', (req, res) => {
     res.render('search-realm');
 });
 
 router.get('/realms/:realmId', (req, res) => {
     res.render('search-realm', {params: req.params.realmId});
+});
+
+// biomes
+router.get('/biomes', (req, res) => {
+    res.render('search-biome');
+});
+
+router.get('/biomes/:biomeId', (req, res) => {
+    res.render('search-biome', {params: req.params.biomeId});
+});
+
+// ecoregions
+router.get('/ecoregions/:ecoId', (req, res) => {
+    res.render('search-ecoregion', {params: req.params.ecoId});
 });
 
 module.exports = router;
