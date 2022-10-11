a1e2c09a592103aca0341776bf175f2ccec93dfa
Finn Stitcher <foundfootage@protonmail.com>
2022-10-10 20:20:11 -0700

    ecoregion route; linked eco and species

---
+++ models/Species.js
@@ -0,0 +1,32 @@
+const {Model, DataTypes} = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Species extends Model {};
+
+Species.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        species_name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        genus_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'species'
+    }
+);
+
+module.exports = Species;
