c5ea599251e152bdb31dc16127c6cd24c1b3e426
Finn Stitcher <foundfootage@protonmail.com>
2022-10-10 19:11:05 -0700

    realms list

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,26 +1,29 @@
 const router = require('express').Router();
 
+const {getRealms} = require('../controllers/realmController');
+const { Realm, Biome, Ecoregion } = require('../models');
+
 // realms
 router.get('/realms', (req, res) => {
-    res.render('search-realm');
+	res.render('list-realms');
 });
 
 router.get('/realms/:realmId', (req, res) => {
-    res.render('search-realm', {params: req.params.realmId});
+	res.render('search-realm', { params: req.params.realmId });
 });
 
 // biomes
 router.get('/biomes', (req, res) => {
-    res.render('search-biome');
+	res.render('search-biome');
 });
 
 router.get('/biomes/:biomeId', (req, res) => {
-    res.render('search-biome', {params: req.params.biomeId});
+	res.render('search-biome', { params: req.params.biomeId });
 });
 
 // ecoregions
 router.get('/ecoregions/:ecoId', (req, res) => {
-    res.render('search-ecoregion', {params: req.params.ecoId});
+	res.render('search-ecoregion', { params: req.params.ecoId });
 });
 
 module.exports = router;
