33578c00044974e8b5bb70cb7c0c60b622e36828
Finn Stitcher <foundfootage@protonmail.com>
2022-10-12 14:32:34 -0700

    sorted ecoregion search

--- routes/ecoRoutes.js
+++ routes/ecoRoutes.js
@@ -5,40 +5,54 @@ const {Ecoregion, Species, Genus, Class, Order, CommonName} = require('../models
 router.get('/:ecoID', async (req, res) => {
     const {ecoID} = req.params;
 
-    // making orders an extra layer in that stack was unacceptably slow
-    const dbResponse = await Class.findAll({
-        attributes: ['id', 'class_name'],
+    // get this ecoregion and all connected species
+    const dbResponse1 = await Ecoregion.findOne({
+        where: {
+            id: ecoID
+        },
+        attributes: ['id', 'ecoregion_name'],
         include: {
             model: Species,
-            attributes: ['id', 'species_name'],
-            include: [
-                {
-                    model: Ecoregion,
-                    as: 'ecoregions',
-                    attributes: ['id', 'ecoregion_name'],
-                    where: {
-                        id: ecoID
-                    }
-                },
-                {
-                    model: Genus,
-                    attributes: ['id', 'genus_name']
-                },
-                {
-                    model: CommonName,
-                    attributes: ['id', 'common_name']
-                },
-                {
-                    model: Order,
-                    attributes: ['id', 'order_name']
-                }
-            ]
+            as: 'species',
+            attributes: ['id', 'species_name']
         }
     });
+    const data1 = dbResponse1.get({plain: true});
 
-    const data = dbResponse.map(element => element.get({plain: true}));
+    // create array of species ids
+    const speciesIds = [];
+    data1.species.forEach(element => {
+        speciesIds.push(element.id);
+    });
 
-	res.render('search-ecoregion', {data});
+    // get class-order-species stack
+    const dbResponse2 = await Class.findAll({
+        attributes: ['id', 'class_name'],
+        include: {
+            model: Order,
+            attributes: ['id', 'order_name'],
+            include: {
+                model: Species,
+                attributes: ['id', 'species_name'],
+                where: {
+                    id: [...speciesIds]
+                },
+                include: [
+                    {
+                        model: Genus,
+                        attributes: ['id', 'genus_name']
+                    },
+                    {
+                        model: CommonName,
+                        attributes: ['id', 'common_name']
+                    }
+                ]
+            }
+        }
+    });
+    const data2 = dbResponse2.map(element => element.get({plain: true}));
+
+	res.render('search-ecoregion', {data: data2});
 });
 
 module.exports = router;
