6d2723a2f34800c3686264b88e63636561ce95ac
Finn Stitcher <foundfootage@protonmail.com>
2022-10-13 22:57:54 -0700

    updated species search

--- routes/speciesRoutes.js
+++ routes/speciesRoutes.js
@@ -5,34 +5,41 @@ const {Genus, Species, Ecoregion, Realm, CommonName} = require('../models');
 router.get('/:speciesID', async (req, res) => {
     const {speciesID} = req.params;
 
-    const dbResponse = await Species.findOne({
-        where: {
-            id: speciesID
-        },
-        attributes: ['id', 'species_name'],
-        include: [
-            {
-                model: CommonName,
-                attributes: ['id', 'common_name']
-            },
-            {
-                model: Genus,
-                attributes: ['id', 'genus_name']
-            },
-            {
-                model: Ecoregion,
-                as: 'ecoregions',
-                attributes: ['id', 'ecoregion_name'],
-                include: {
-                    model: Realm,
-                    attributes: ['id', 'realm_name']
-                }
+    // get realm-ecoregion-species-species data stack
+    const dbResponse = await Realm.findAll({
+        attributes: ['id', 'realm_name'],
+        include: {
+            model: Ecoregion,
+            attributes: ['id', 'ecoregion_name'],
+            include: {
+                model: Species,
+                as: 'species',
+                attributes: ['id', 'species_name'],
+                where: {
+                    id: speciesID
+                },
+                include: [
+                    {
+                        model: Genus,
+                        attributes: ['id', 'genus_name']
+                    },
+                    {
+                        model: CommonName,
+                        attributes: ['id', 'common_name']
+                    }
+                ]
             }
-        ]
+        }
     });
-    const data = dbResponse.get({plain: true});
+    const data = dbResponse.map(element => element.get({plain: true}));
+    
+    // get rid of any realms where this species is not found
+    const cleanedData = data.filter(element => element.ecoregions[0]);
+    
+    // extract species data
+    const [speciesData] = cleanedData[0].ecoregions[0].species;
 
-    res.render('search-species', {data});
+    res.render('search-species', {data: cleanedData, speciesData});
 });
 
 module.exports = router;
