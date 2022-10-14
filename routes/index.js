7455e148b7f02f94ec1a53179c679ea32eee99f4
Finn Stitcher <foundfootage@protonmail.com>
2022-10-13 22:35:55 -0700

    search by family

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,7 @@ router.use('/ecoregions', require('./ecoRoutes'));
 
 router.use('/classes', require('./classRoutes'));
 router.use('/orders', require('./orderRoutes'));
+router.use('/families', require('./familyRoutes'));
 router.use('/genera', require('./genusRoutes'));
 router.use('/species', require('./speciesRoutes'));
 
